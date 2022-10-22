e0ad2f9a2b53beb797571292b3239ccc6ca39d7b
hust_tianbo <nothing_tb@foxmail.com>
2022-10-22 10:14:24 +0800

    fix length id

--- lib/gene_id.go
+++ lib/gene_id.go
@@ -12,6 +12,6 @@ func init() {
 
 func GeneID(personId string) string {
 	unixNano := time.Now().Unix()
-	randInt := rand.Intn(10000)
-	return fmt.Sprintf("%s_%d_%d", personId, unixNano, randInt)
+	randInt := rand.Intn(1000)
+	return fmt.Sprintf("%s_%d_%03d", personId, unixNano, randInt)
 }
